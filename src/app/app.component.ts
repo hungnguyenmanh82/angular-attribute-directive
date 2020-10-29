eb8557cb3c78308e4558d846268f24033f403070
DESKTOP-LBOHS1J\hungnm2 <hung.nguyen@netvisiontel.com>
2020-10-29 08:51:31 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { TestDirectiveDirective } from './test-directive.directive';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'b4-directive';
+
+  /**
+   * <p appTestDirective #variable1="testDirective">
+   * cách gọi #varaible từ TypeScript giống hệt component
+   */
+  @ViewChild('variable1') directive: TestDirectiveDirective;
+  public highlight(color: string) {
+    this.directive.highlight(color);
+  }
 }
